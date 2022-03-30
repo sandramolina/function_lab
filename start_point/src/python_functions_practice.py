ad3f47872b9c145dced1fa4202923e39c0a47a05
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-03-30 13:24:38 +0100

    first commit

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,54 @@
+def return_10():
+    return 10
+
+def add(first_num, second_num):
+    return first_num + second_num
+
+def subtract(first_num, second_num):
+    return first_num - second_num
+
+def multiply(first_num, second_num):
+    return first_num * second_num
+
+def divide(first_num, second_num):
+    return first_num / second_num
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(string_1, string_2):
+    string_1 = int(string_1)
+    string_2 = int(string_2)
+    return string_1 + string_2
+
+months = {
+    1: "January",
+    3: "March",
+    9: "September"
+}
+def number_to_full_month_name(key):
+    return months[key]
+
+
+months_short = {
+    1: "Jan",
+    4: "Apr",
+    10: "Oct"
+}
+def number_to_short_month_name(key):
+    return months_short[key]
+
+import math
+def volume_of_cube_calculator(cube_side):
+   return int(math.pow(cube_side, 3))
+
+def string_reverser(string):
+    return string[::-1]
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return int(celsius)
+
